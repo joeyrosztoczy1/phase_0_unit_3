d62f195a049debe24758af3309d152fc3f0e9047
Joey Rosztoczy <joeyr@stotzeq.com>
2014-05-14 17:56:10 -0700

    dat solo challenge dun dun dun

--- week_7/3_model_one_to_many_solo_challenge/my_solution.md
+++ week_7/3_model_one_to_many_solo_challenge/my_solution.md
@@ -3,18 +3,49 @@
 ## Release 0: Users Fields
 <!-- Identify the fields Twitter collects data for -->
 
+Tweets
+Replies
+Following
+Followers
+Favorites
+Name
+Biography
+Location
+Website
+Theme_Color 
+
+
+
 ## Release 1: Tweet Fields
 <!-- Identify the fields Twitter uses to represent/display a tweet. What are you required or allowed to enter? -->
 
+Text
+Photo
+Location
+Time
+User
+
+
 ## Release 2: Explain the relationship
 The relationship between `users` and `tweets` is: 
-<!-- because... -->
+<!-- one user to many tweets -->
 
 ## Release 3: Schema Design
 <!-- Include your image (inline) of your schema -->
+![alt tweet_schema_image](week_7/imgs/solo_challenge.png "Tweet_Schema")
+
 
 ## Release 4: SQL Statements
 <!-- Include your SQL Statements. How can you make markdown files show blocks of code? -->
 
+```SQL 
+SELECT tweets FROM Users JOIN Tweets ON Tweets_ID = tweets.id WHERE Users.id = 1
+SELECT tweets FROM Users JOIN Tweets ON Tweets_ID = tweets.id WHERE Users.id = 1 AND created_at > 05/07/2014 
+SELECT tweets FROM Users JOIN Tweets ON Tweets_ID = tweets.id WHERE name = 'insert_handle'
+SELECT name FROM Users JOIN Tweets ON Tweets_ID = tweets.id WHERE Tweets.id = 1
+```
+
+
 ## Release 5: Reflection
 <!-- Be sure to add your reflection here!!! -->
+This solo challenge was significantly simpler than the quizes and querying we did from SQLZoo. It would have been an easier transition learning by doing this first as all the SQL Queries can be accomplished by a simple JOIN. (I think... I have been querying SQL for work, but maybe my statements up there don't work and I thought it was easier than it really is). I think visualizing db's with an editor like the one from Socrates can be beneficial if we are using large interacting systems like dozens of tables with dozens of fields. For most basic stuff though it seems to slow development down quite a bit.
--- week_7/7_gps3.1/our_solution.rb
+++ week_7/7_gps3.1/our_solution.rb
@@ -1,34 +1,78 @@
-# U3.W7: Build an Electronic Grocery List!
- 
-# Your full names:
-# 1.
-# 2.
- 
-# User Stories (As many as you want. Delete the statements you don't need)
-# As a user, I want to
-# As a user, I want to
-# As a user, I want to
-# As a user, I want to
-# As a user, I want to
- 
-# Pseudocode
-# 
-# 
-# 
-# 
-# 
-# 
- 
- 
-# Your fabulous code goes here....
+# U3.W7: GPS 3.1 Create an Electronic Grocery List
+# Learning Competencies:
+# Implement a basic Ruby class and identify when to use instance variables
+# Identify the attributes and methods of a class based on user stories
+# Identify tradeoffs between simple OO design patterns
+# Summary
+# In this challenge, you will create an electronic grocery list command-line application. Show off the object-oriented design principles you learned last week. You should spend time discussing tradeoffs with different designs. This is your grocery list, so get creative and have fun, but also make it great code!
+
+# Release 0: User Stories
+# With your partner, talk about what you would like to do with your grocery list. Start each statement with "As a user, I want to ..." These are what we call "user stories."
+# As users we would like to be able to add items to the list and a quantity for each item. We would also want to be able to
+# check off items once they have been put into the cart. we would want to retunr/display the current list. be able to 
+# remove items from the list. Be able to name/date the list.
+#
 
 
+# Release 1: Pseudocode
+# create a class 'grocery_list'
+# intialize the with grocery_item as argument
+# create a hash 'grocery_item' 
+# create a method to add grocery_item to list
+# create a method to delete grocery_item from list
+#
 
+# Release 2: Write Driver Code
+def assert
+    raise ArgumentError, "We have a problem at this line!" unless yield
+end
 
+# Release 3: Initial Solution
+# Create your initial solution.
 
+class Grocery_List
+    
+    def initialize
+        @grocery_list = {}
+    end
+    
+    def add_item(item, quantity)
+        @grocery_list[item] = quantity
+        puts @grocery_list
+    end
+    
+    def clear_list
+        @grocery_list = {}
+    end
+    
+    def subtract_item(subtracted_item)
+        @grocery_list.delete_if { |key, value| key == subtracted_item } 
+    end
+    
+     def check_off_list(checked_off_item)
+        puts 'Checked off!'
+        @grocery_list.each_key do |key|
+             checked_off_item == key  ? @grocery_list[key] = "Done!" : "not here!"
+        end
+    end
+    
+    def show_list
+        puts @grocery_list
+    end
+    
+end
 
+a = Grocery_List.new
+a.add_item("apples", 2)
+a.show_list
+a.check_off_list("apples")
+a.show_list
 
-# DRIVER CODE GOES HERE. 
- 
- 
- 
+# Release 4: Refactor your solution
+# With your partner, review the code. Is it DRY? Are the names good?
+
+# Release 5: Reflect
+# Copy your code from stypi into the our_solution.rb file. Then complete a reflection in the designated section.
+
+# Release 6: Sync your changes to Github!
+# Release 6: Submit Feedback.
